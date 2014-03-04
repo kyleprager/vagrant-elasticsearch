a119db474032125bce776634c6150034b013c699
kyleprager <kyleprager@gmail.com>
2014-03-04 03:08:12 -0800

    elasticsearch download, untar, cluster name config, network host config, and startup taken care of in Vagrantfile.  Puppet file default.pp still checks and installs openjdk-7-jre if necessary.

--- Vagrantfile
+++ Vagrantfile
@@ -44,17 +44,52 @@ Vagrant.configure("2") do |config|
 
       node.vm.box = "precise64"
 
-      node.vm.network :private_network, ip: ipAddrPrefix + num.to_s
+      ipaddr = ipAddrPrefix + num.to_s
+
+      # setup private network
+      node.vm.network :private_network, ip: ipaddr
+
+      # write this computer's IP to disk
+      elasticsearchConfigFileLocation = "/home/vagrant/elasticsearch-1.0.1/config/elasticsearch.yml"
+      node.vm.provision :shell, inline: 
+      "
+      cd /home/vagrant
+
+      if [ -a elasticsearch-1.0.1.tar.gz ]
+      then echo 'Elasticsearch already downloaded and unzipped.';
+      else 
+        wget https://download.elasticsearch.org/elasticsearch/elasticsearch/elasticsearch-1.0.1.tar.gz 2>&1;
+        tar xf elasticsearch-1.0.1.tar.gz
+      fi
+
+      if grep es.ycsb.cluster $2
+      then echo 'Elasticsearch cluster name already set.';
+      else echo cluster.name: es.ycsb.cluster >> $2;
+      fi
+
+      if grep $1 $2
+      then echo 'Elasticsearch network.host property already set.';
+      else echo network.host: $1 >> $2;
+      fi
+
+      if ps aux | grep elasticsearch | grep -v grep
+      then echo 'Elasticsearch already running.';
+      else 
+        ./elasticsearch-1.0.1/bin/elasticsearch -d;
+        echo 'Started elasticsearch.'
+      fi
+      ", 
+      :args => "#{ipaddr} #{elasticsearchConfigFileLocation}"
 
       node.vm.provider "virtualbox" do |v|
 
         v.name = "Elasticsearch Node " + num.to_s
 
+      end
+
     end
 
   end
-
-end
 
 
 end
--- manifests/default.pp
+++ manifests/default.pp
@@ -1,4 +1,3 @@
-
 # update apt-get to pull latest list of ubuntu packages
 exec { "apt-update":
 
@@ -14,73 +13,5 @@ package { "openjdk-7-jre":
 	ensure => installed,
 
 	require => Exec["apt-update"]
-
-}
-
-# download elasticsearch 1.0.1
-exec { "elasticsearch-download": 
-
-  command => "/usr/bin/wget https://download.elasticsearch.org/elasticsearch/elasticsearch/elasticsearch-1.0.1.tar.gz",
-
-  cwd => "/home/vagrant/",
-
-  creates => "/home/vagrant/elasticsearch-1.0.1.tar.gz",
-
-  require => Package["openjdk-7-jre"]
-
-}
-
-# untar elasticsearch 1.0.1
-exec { "untar-elasticsearch":
-
-  command => "/bin/tar xf elasticsearch-1.0.1.tar.gz",
-
-  cwd => "/home/vagrant/",
-
-  creates => "/home/vagrant/elasticsearch-1.0.1",
-
-  require => Exec["elasticsearch-download"]
-
-}
-
-# set the clustername so the VMs all join the same cluster
-exec { "set-clustername-elasticsearch":
-
-  command => "/bin/echo 'cluster.name: es.ycsb.cluster' >> elasticsearch-1.0.1/config/elasticsearch.yml",
-
-  cwd => "/home/vagrant/",
-
-  unless => "/bin/grep es.ycsb.cluster elasticsearch-1.0.1/config/elasticsearch.yml",
-
-  require => Exec["untar-elasticsearch"]
-
-}
-
-# set the network.host setting in elasticsearch.yaml config file.  If this is not done the VM loads with eth0's IP.
-# we need it to load with eth1's IP which looks like 192.168.255.10[0-9]
-exec { "set-network-host-elasticsearch":
-
-  command => "ifconfig eth1 | grep inet | grep -v inet6 | grep -o 192.168.255.10[0-9] | sed -e 's/\(.*\)/network.host: \1/'  >> elasticsearch-1.0.1/config/elasticsearch.yml",
-
-  path => "/sbin:/bin:/usr/bin",
-
-  cwd => "/home/vagrant/",
-
-  unless => "/bin/grep -E '^network.host' elasticsearch-1.0.1/config/elasticsearch.yml",
-
-  require => Exec["set-clustername-elasticsearch"]
-
-}
-
-# start elasticsearch in the background
-exec { "run-elasticsearch":
-
-  command => "/usr/bin/sudo ./elasticsearch-1.0.1/bin/elasticsearch -d",
-
-  cwd => "/home/vagrant/",
-
-  unless => "/bin/ps aux | grep elasticsearch | grep -v grep",
-
-  require => Exec["set-network-host-elasticsearch"]
 
 }
